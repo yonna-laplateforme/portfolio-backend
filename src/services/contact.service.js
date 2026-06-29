3627d2287714a2aa267dc26bbf551c6e7a0b40c0
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-06-29 19:47:27 +0200

    mailjetconfig

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -1,41 +1,23 @@
-import nodemailer from 'nodemailer';
-import AppError from '../errors/AppError.js';
-
 export const sendContactEmail = async ({ name, email, message }) => {
-  
-  // 💡 DÉPLACEMENT ICI : Le transporteur est créé à la demande, les variables .env sont lues à coup sûr !
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, 
+    host: 'in-v3.mailjet.com', 
+    port: 587,
+    secure: false, 
     auth: {
-      user: process.env.MAIL_USER, // yonna.s.merlini@gmail.com
-      pass: process.env.MAIL_PASS, // pekyuirstnlkdrhr
+      user: process.env.MAIL_USER, 
+      pass: process.env.MAIL_PASS,
     },
   });
 
   const mailOptions = {
-    from: `"${name}" <${process.env.MAIL_USER}>`, 
+    from: `"Ton Portfolio" <yonna.s.merlini@gmail.com>`, 
     replyTo: email, 
     to: process.env.MAIL_TO,
     subject: `Nouveau message Portfolio de ${name}`,
-    text: `Vous avez reçu un message de : ${name} (${email})\n\nMessage :\n${message}`,
-    html: `
-      <h3>Nouveau message depuis votre Portfolio</h3>
-      <p><strong>Nom :</strong> ${name}</p>
-      <p><strong>Email :</strong> ${email}</p>
-      <p><strong>Message :</strong></p>
-      <p style="white-space: pre-wrap;">${message}</p>
-    `,
+    text: `Message de : ${name} (${email})\n\n${message}`,
+    html: `<h3>Nouveau message</h3><p>${message}</p>`
   };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log('✉️ Email envoyé avec succès par le Service !');
-    return true;
-  } catch (error) {
-    console.error('Erreur Nodemailer directe :', error);
-    throw new AppError("L'envoi de l'email a échoué. Veuillez réessayer plus tard.", 500);
-  }
+  await transporter.sendMail(mailOptions);
+  return true;
 };

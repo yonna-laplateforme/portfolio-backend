85caeac4a39e3b1f19eea65934cdb327a7031486
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-06-29 14:37:43 +0200

    render

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,16 @@
+import mysql from 'mysql2';
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }
 });
+
+// C'est cette ligne qui résout le problème : 
+// Elle force mysql2 à utiliser les promesses partout.
+const promisePool = pool.promise();
+
+export default promisePool;

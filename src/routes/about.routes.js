85317f63afc0dcc3607723a95d1334050b51979d
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-07-07 19:05:08 +0200

    correction

--- src/routes/about.routes.js
+++ src/routes/about.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as aboutController from '../controllers/about.controller.js';
 import { authenticate, authorize } from '../middlewares/auth.middleware.js';
-import { uploadImage, uploadVideo } from '../middlewares/upload.js';
+import { uploadImage, uploadVideo } from '../middlewares/upload.middleware.js';
 
 const router = Router();
 

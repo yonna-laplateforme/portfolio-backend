d41db649320590af09762314220e432cc9563115
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-07-07 21:06:14 +0200

    helmet debug

--- src/server.js
+++ src/server.js
@@ -12,28 +12,21 @@ import errorHandler from './middlewares/errorHandler.js';
 import technologyRoutes from './routes/technology.routes.js';
 
 const app = express();
-// app.use(
-// helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         "default-src": ["'self'"],
-//         "script-src": ["'self'", "'unsafe-inline'"],
-//         "style-src": ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
-//         "font-src": ["'self'", "fonts.gstatic.com"],
-//         "img-src": ["'self'", "data:", "https://portfolio-backend-7xj4.onrender.com"],
-//         "connect-src": ["'self'", "https://portfolio-backend-7xj4.onrender.com"],
-//         "media-src": ["'self'", "https://res.cloudinary.com"],
-//       },
-//     },
-//   })
-// );
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
-  );
-  next();
-});
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "default-src": ["'self'", "https://res.cloudinary.com"],
+        "media-src": ["'self'", "https://res.cloudinary.com", "blob:", "data:"],
+        "script-src": ["'self'", "'unsafe-inline'"],
+        "style-src": ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
+        "img-src": ["'self'", "data:", "https://res.cloudinary.com"],
+        "connect-src": ["'self'", "https://portfolio-backend-7xj4.onrender.com"],
+      },
+    },
+  })
+);
+
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json({ limit: '10mb' }));
 

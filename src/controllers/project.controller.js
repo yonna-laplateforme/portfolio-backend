9c3b0d7f4868ff4492a4f62efe44bfd2648e3d72
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-07-16 13:46:20 +0200

    retrait try catch

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,124 +1,99 @@
 import * as ProjectService from '../services/project.service.js';
+import AppError from '../errors/AppError.js';
 
 /**
  * Récupérer tous les projets
  */
-export const getAllProjects = async (req, res, next) => {
-    try {
-        const projects = await ProjectService.getAllProjects();
-        return res.json(projects);
-    } catch (error) {
-        return next(error);
-    }
+export const getAllProjects = async (req, res) => {
+  const projects = await ProjectService.getAllProjects();
+  return res.json(projects);
 };
 
 /**
- * Récupérer les projets affichés sur la home
+ * Récupérer les projets affichés sur la page d'accueil
  */
-export const getProjectsForHome = async (req, res, next) => {
-    try {
-        const projects = await ProjectService.getProjectsForHome();
-        return res.json(projects);
-    } catch (error) {
-        return next(error);
-    }
+export const getProjectsForHome = async (req, res) => {
+  const projects = await ProjectService.getProjectsForHome();
+  return res.json(projects);
 };
 
 /**
  * Récupérer un projet par son ID
  */
-export const getProjectById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const project = await ProjectService.getProjectById(id);
+export const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  const project = await ProjectService.getProjectById(id);
 
-        if (!project) {
-            return res.status(404).json({ error: 'Projet introuvable' });
-        }
+  if (!project) {
+    throw new AppError('Projet introuvable', 404);
+  }
 
-        return res.json(project);
-    } catch (error) {
-        return next(error);
-    }
+  return res.json(project);
 };
 
 /**
  * Créer un nouveau projet
  */
-export const createProject = async (req, res, next) => {
-    try {
-        const projectData = { ...req.body };
+export const createProject = async (req, res) => {
+  const projectData = { ...req.body };
 
-        projectData.isFeatured =
-            req.body.isFeatured === 'true' ||
-                req.body.isFeatured === 1 ||
-                req.body.isFeatured === '1'
-                ? 1
-                : 0;
+  projectData.isFeatured =
+    req.body.isFeatured === 'true' ||
+    req.body.isFeatured === 1 ||
+    req.body.isFeatured === '1'
+      ? 1
+      : 0;
 
-        if (req.files && req.files.length > 0) {
-            projectData.image_urls = req.files.map((file) => file.path);
-        } else {
-            projectData.image_urls = [];
-        }
+  projectData.image_urls =
+    req.files?.length > 0
+      ? req.files.map((file) => file.path)
+      : [];
 
-        const project = await ProjectService.createProject(projectData);
+  const project = await ProjectService.createProject(projectData);
 
-        return res.status(201).json(project);
-    } catch (error) {
-        return next(error);
-    }
+  return res.status(201).json(project);
 };
 
 /**
  * Modifier un projet existant
  */
-export const updateProject = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+export const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const currentProject = await ProjectService.getProjectById(id);
 
-        const currentProject = await ProjectService.getProjectById(id);
+  if (!currentProject) {
+    throw new AppError('Projet introuvable', 404);
+  }
 
-        if (!currentProject) {
-            return res.status(404).json({ error: 'Projet introuvable' });
-        }
+  const projectData = { ...req.body };
 
-        const projectData = { ...req.body };
+  projectData.isFeatured =
+    req.body.isFeatured === 'true' ||
+    req.body.isFeatured === 1 ||
+    req.body.isFeatured === '1'
+      ? 1
+      : 0;
 
-        projectData.isFeatured =
-            req.body.isFeatured === 'true' ||
-                req.body.isFeatured === 1 ||
-                req.body.isFeatured === '1'
-                ? 1
-                : 0;
+  const existingImages = currentProject.image_url
+    ? currentProject.image_url.split(',')
+    : [];
 
-        const existingImages = currentProject.image_url
-            ? currentProject.image_url.split(',')
-            : [];
+  const newFilePaths =
+    req.files?.length > 0
+      ? req.files.map((file) => file.path)
+      : [];
 
-        if (req.files && req.files.length > 0) {
-            const newFilePaths = req.files.map((file) => file.path);
-            projectData.image_urls = [...existingImages, ...newFilePaths];
-        } else {
-            projectData.image_urls = existingImages;
-        }
+  projectData.image_urls = [...existingImages, ...newFilePaths];
 
-        const project = await ProjectService.updateProject(id, projectData);
+  const project = await ProjectService.updateProject(id, projectData);
 
-        return res.json(project);
-    } catch (error) {
-        return next(error);
-    }
+  return res.json(project);
 };
 
 /**
  * Supprimer un projet
  */
-export const deleteProject = async (req, res, next) => {
-    try {
-        await ProjectService.deleteProject(req.params.id);
-        return res.status(204).send();
-    } catch (error) {
-        return next(error);
-    }
+export const deleteProject = async (req, res) => {
+  await ProjectService.deleteProject(req.params.id);
+  return res.status(204).send();
 };

2198595315c274ee6b70f0e5baa6d8d15d01eba7
Yonna Merlini <yonna.merlini@laplateforme.io>
2026-09-07 11:11:15 +0200

    connec

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ app.use(
           "'self'",
           "https://api.cloudinary.com",
           "https://res.cloudinary.com",
-          "https://api.www-yonnamerlini.com",
+          "https://api.yonnamerlini.com",
         ],
       },
     },
